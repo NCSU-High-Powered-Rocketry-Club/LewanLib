0e47a368b73e9e00625c2a13145a7db4a560805f
pranitlokineni <166269669+pranitlokineni@users.noreply.github.com>
2026-08-24 14:38:03 -0400

    updated script

--- tests/scripts/servo_test.py
+++ tests/scripts/servo_test.py
@@ -5,37 +5,25 @@ port = "/dev/ttyAMA0"
 servo_id = 1
 
 with ServoBus(port=port, baudrate=115200, on_exit_power_off=False) as bus:
-    print("here")
     servo = bus.get_servo(servo_id)
-    # Write tests
-    # servo.angle_offset_adjust(5.0)
-    # servo.angle_offset_write()
-    # servo.angle_limit_write(10.0, 230.0)
-    # servo.vin_limit_write(6.0, 12.0)
-    # servo.temp_max_limit_write(75.0)
-    servo.id_write(servo_id)  # Re-write same ID for test
-    print("Rewrote id")
-    
-    # Move tests 
-    servo.set_powered(True)
-    # Don't wait for the full move (wait=False), but give a tiny delay for bus stability
-    servo.move_time_write(120.0, 0.0, wait=False)
-    time.sleep(0.05) 
-    
-    print("move time read ", servo.move_time_read())
-    time.sleep(0.05)
-    servo.move_stop()
-
-
-    # Read tests
-    print("angle offset ", servo.angle_offset_read())
-    print("angle limit ", servo.angle_limit_read())
-    print("vin limit ", servo.vin_limit_read())
-    print("temp max limit ", servo.temp_max_limit_read())
-    print("temp read c ", servo.temp_read(units='C'))
-    print(servo.temp_read(units='F'))
-    print("voltage in read ", servo.vin_read())
-    print("servo pos read ", servo.pos_read())
-    print(servo.mode_read())
-
-    servo.set_powered(False)
+    current_angle = servo.pos_read()
+    try:
+        while True:
+            current_extension = servo.pos_read()
+            print(f"Current extension: {current_extension}")
+            print("Up arrow for extension, down arrow for retraction")
+            match str(input()):
+                case "up":
+                    servo.move_time_write(current_angle + 5, 1)
+                    current_angle += 5
+                case "down":
+                    servo.move_time_write(current_angle - 5, 1)
+                    current_angle -= 5
+                case _:
+                    print("Invalid input")
+                    continue
+            print(f"Current Voltage: {servo.vin_read()}")
+            print(f"Current Temp: {servo.temp_read()}")
+            print(f"")
+    except KeyboardInterrupt:
+        pass
