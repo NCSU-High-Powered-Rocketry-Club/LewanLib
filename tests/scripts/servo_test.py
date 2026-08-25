ea5348340518921241d27e3d442b91e0b1f2a705
harshil21 <hoppingturtles@proton.me>
2026-08-25 14:46:01 -0400

    updated script

--- tests/scripts/servo_test.py
+++ tests/scripts/servo_test.py
@@ -1,4 +1,5 @@
 import time
+import msgspec
 from lewanlib.bus import ServoBus
 from sshkeyboard import listen_keyboard
 
@@ -10,11 +11,12 @@ current_extension = 10  # starting position
 # Callback for key presses
 def press(key):
     global current_extension
-    if key == "up":
+    current_extension = servo.pos_read() 
+    if key == "right":
         current_extension += 20
         servo.move_time_write(current_extension, .25, wait=False)
         print(f"Moved up to {current_extension}")
-    elif key == "down":
+    elif key == "left":
         current_extension -= 20
         servo.move_time_write(current_extension, .25, wait=False)
         print(f"Moved down to {current_extension}")
@@ -22,11 +24,15 @@ def press(key):
         print("Exiting...")
         return False  # stop listener
 
+    packet = servo.return_data_packet()
+    print("Servo data packet:")
+    print(msgspec.structs.asdict(packet))
+
 # Initialize servo bus
 with ServoBus(port=port, baudrate=115200, on_exit_power_off=False) as bus:
     servo = bus.get_servo(servo_id)
     servo.id_write(servo_id)
     servo.set_powered(True)
 
-    print("Use arrow keys to move servo. Press 'q' to quit.")
+    print("Use left and right arrow keys to move servo. Press 'q' to quit.")
     listen_keyboard(on_press=press)
